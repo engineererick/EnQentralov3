d7e75f89bd93500f343e46623c932c5a86371edd
Erick <engineererick@hotmail.com>
2018-12-05 00:39:58 -0600

    Parte19

--- EnQentralov3/EnQentralov3/ViewModels/MainViewModel.cs
+++ EnQentralov3/EnQentralov3/ViewModels/MainViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using EnQentralov3.Services;
+using EnQentralov3.Views;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace EnQentralov3.ViewModels
 {
@@ -12,6 +16,19 @@ namespace EnQentralov3.ViewModels
         public MainViewModel()
         {
             this.Publics = new PublicacionesViewModel();
+        }
+
+        public ICommand BuscaCommand
+        {
+            get
+            {
+                return new RelayCommand(GoToBusca);
+            }
+        }
+
+        private async void GoToBusca()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new BuscaPage());
         }
     }
 }
